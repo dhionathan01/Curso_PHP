56ca8eb43fd450d2a0498ab6140411bcb6f79cc8
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-03-14 20:35:48 -0300

    2 - Tag Curta

---
+++ Seção 11 - PHP 7/2 - Habilitando tags Curtas/index.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Habilitando Tags curtas</title>
+    <!-- OBS: AS TAGS CURTAS NÃO ESTÁO MAIS DISPONÍVEIS NA VERSÃO DO PHP 8 -->
+</head>
+<body>
+    <? echo 'Utilizando a tag curta' ?>
+</body>
+</html>
