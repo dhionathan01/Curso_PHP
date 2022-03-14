bf223e36f4d4dfd001351177c053ceab32c85cc9
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-03-14 20:47:18 -0300

    3 - Echo e print

---
+++ Seção 11 - PHP 7/3 - Echo and print/index.php
@@ -0,0 +1,16 @@
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Habilitando Tags curtas</title>
+    <!-- OBS: AS TAGS CURTAS NÃO ESTÁO MAIS DISPONÍVEIS NA VERSÃO DO PHP 8 -->
+</head>
+<body>
+    <?php 
+        echo 'Comando Echo <br>';
+
+       echo print 'Comando print <hr>';
+    ?>
+</body>
+</html>
