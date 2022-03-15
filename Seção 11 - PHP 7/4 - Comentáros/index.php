e8586b939cb348469af0b4eaeb14030e92d4b6f0
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-03-14 21:54:37 -0300

    4 - Comentários

---
+++ Seção 11 - PHP 7/4 - Comentáros/index.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Comentatários</title>
+</head>
+<body>
+    <!-- Comentários -->
+    <?php
+        // Comentário de uma linha
+        echo 'Teste 1 <br>';
+        # Comentário de uma linha
+        print("Teste 2 ");
+        /* 
+        Comentários de múltiplas linhas.
+        Este tipo de comentário permite que várias 
+        linhas sejam comentadas ao mesmo tempo.
+        */
+    ?>
+</body>
+</html>
