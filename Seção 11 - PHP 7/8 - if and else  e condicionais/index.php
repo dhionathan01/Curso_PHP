8cb938075bd290895bac17768c308798fd28400f
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-03-20 19:00:17 -0300

    8 -  If e else condicionais

---
+++ Seção 11 - PHP 7/8 - if and else  e condicionais/index.php
@@ -0,0 +1,26 @@
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>If/Else - PHP</title>
+</head>
+<body>
+    <?php 
+        // == Verifica se os valores são iguais
+        // === Verifica se os valores são iguais e do mesmo tipo
+        // != ou <> Verifica se os valores são diferentes
+        // !==  Verifica se os valores e o tipo são diferentes
+        // < verifica se o valor a esquerda é menor que o da direita
+        // > verifica se o valor a direita é menor que o da esquerda
+        // <= verifica se o valor a esquerda é menor ou igual que o da direita
+        // >= verifica se o valor a direita é menor ou igual que o da esquerda
+        if(5 <> 7) {
+            echo 'Verdadeiro';
+        }else{ // o else é opcional
+            echo 'Falso';
+        }
+        
+    ?>
+</body>
+</html>
