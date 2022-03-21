c5ab68273cbaab2617a719743fa92cbacbab301a
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-03-21 16:20:21 -0300

    17 - Operadores Aritméticos

---
+++ Seção 11 - PHP 7/17 - Operadores Aritméticos na atribuição/index.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores aritméticos na atribuição, incremento, decremento</title>
+</head>
+<body>
+    <?php 
+        // Atribuição  ' = '
+        // Aritméticos '+ , - , * , / , % '
+
+        // Operadores aritméticos na atribuição Exemplo: 
+        $x = 10;
+        $y = 8;
+        $x += 3;    /* Memas coisa que $x = $x + 3; */
+        $x += $y;  /* Memas coisa que $x = $x + $y; */
+        echo $x;
+
+        
+
+    ?>
+    
+</body>
+</html>
