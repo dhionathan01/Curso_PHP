74758a72b7691a278b8235530a273ee75fa0fb61
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-03-21 14:20:04 -0300

    13 - Atividade Fixação

---
+++ Seção 11 - PHP 7/13 - Atividade Fixação/index.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade Fixação</title>
+</head>
+<body>
+    <?php 
+        $idade = 20;
+        $peso = 90;
+        $pode_doar = false;
+
+        if(($idade >= 16 and $idade <= 69) and $peso >= 50){
+            echo 'Atende aos requisitos';
+        }else{
+            echo 'Não atendo aos requisitos';
+        }
+    ?>
+</body>
+</html>
