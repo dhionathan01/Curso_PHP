a75877a6badb163e245f265dea78ec9947944621
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-03-28 20:29:57 -0300

    25 - Array Multidmensional

--- Seção 11 - PHP 7/25 - Array Multidimensional/index.php
+++ Seção 11 - PHP 7/25 - Array Multidimensional/index.php
@@ -6,6 +6,29 @@
     <title>Array multidimensional</title>
 </head>
 <body>
-    
+
+    <?php 
+        $lista_coisas = [];
+        $lista_coisas['frutas'] = 'Banana';
+        $lista_coisas['frutas'] = 'Maçã';
+        echo '<pre>';
+        print_r($lista_coisas);
+        echo '</pre>';
+        // Criando um Array associativo:
+        echo 'Criando um Array Associativo';
+        //$lista_coisas['frutas'] =array();
+
+        $lista_coisas['frutas'] = [1 => 'Banana', 2 => 'Maçã', 3=> 'Morango', 4=> 'Uva'];
+        $lista_coisas['pessoas'] = array(1=> 'João', 2 => 'José', 3 => 'Maria');
+        echo '<pre>';
+        print_r($lista_coisas);
+        echo '</pre>';
+
+        echo '<hr>';
+        echo $lista_coisas['frutas'][3];
+        echo '<br>';
+        echo $lista_coisas['pessoas'][2];
+    ?>
+
 </body>
 </html>
