defb8573e0ee478b1b3e259c7d617b6e839bda8c
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-03-29 20:45:01 -0300

    29.3 -  Loops  : For

---
+++ Seção 11 - PHP 7/29 - Loops/29.3 For/index.php
@@ -0,0 +1,20 @@
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - for</title>
+</head>
+<body>
+
+<?php 
+
+    for ($i = 1; $i <= 10; $i++){
+        echo "$i <br>";
+        // Continue e o break funcionam da mesma forma como no while
+    }
+
+?>
+    
+</body>
+</html>
