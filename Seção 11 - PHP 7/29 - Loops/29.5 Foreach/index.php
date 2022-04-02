097a6bfaa2101d57d9ecf4e0532fe863b477b8eb
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-04-02 20:53:10 -0300

    29.5 -  Introdução Foreach

---
+++ Seção 11 - PHP 7/29 - Loops/29.5 Foreach/index.php
@@ -0,0 +1,28 @@
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Foreach</title>
+</head>
+<body>
+    <?php 
+
+        $itens = ['sofá', 'mesa', 'cadeira', 'fogão', 'geladeira'];
+        echo '<pre>';
+        print_r($itens);
+        echo '<pre>';
+
+        foreach($itens as $item){
+            echo"$item";
+            
+            if($item == 'mesa'){
+                echo' (* Compre uma mesa e ganhe 25% de desconto na compra de 4  cadeiras)';
+            }
+
+            echo '<br>';
+        }
+                
+    ?>
+</body>
+</html>
