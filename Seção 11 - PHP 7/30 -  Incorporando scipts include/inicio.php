6a71a2bb9a56c6e0a3e9d089eb4a1350a5a7a6f9
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-04-05 20:32:37 -0300

    30 -  Incorporando Scripts

---
+++ Seção 11 - PHP 7/30 -  Incorporando scipts include/inicio.php
@@ -0,0 +1,15 @@
+<?php
+    //include('menu.php'); // Caso não consiga encontrar o script cria um warning (um aviso)
+    //require ('menu.php'); // Caso não consiga encontrar o script ocorre um fatal error.
+    include_once('menu.php'); // Funciona da mesma forma que o include porém só é executado uma única vez obs abaixo:
+
+
+?>
+
+contúdo da página(início)
+<br>
+<?php
+    //include('menu.php'); // Caso não consiga encontrar o script cria um warning (um aviso)
+    //require ('menu.php'); // Caso não consiga encontrar o script ocorre um fatal error.
+    include_once('menu.php');
+?>
