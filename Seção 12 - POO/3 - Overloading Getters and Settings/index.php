48ed50f009ff892cb5cd48399a60ff5ab8522262
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-05-03 16:08:56 -0300

    Seção 12 POO - 4 Chamando métodos internamente

--- Seção 12 - POO/3 - Overloading Getters and Settings/index.php
+++ Seção 12 - POO/3 - Overloading Getters and Settings/index.php
@@ -16,6 +16,16 @@ use Funcionario as GlobalFuncionario;
         function __get($atributo){
             return $this->$atributo;
         }
+
+        // Métodos
+        function resumeCadFunc(){
+            return $this->__get('nome') . ' possui ' . $this->__get('numFilhos') .  ' filho(s)';
+       }
+
+       function modificarNumFilhos($numFilhos){
+        $this->numFilhos = $numFilhos;
+    }
+
     }
 
     $funcionario1 = new GlobalFuncionario();
@@ -24,6 +34,17 @@ use Funcionario as GlobalFuncionario;
     $funcionario1->__set("numFilhos", 2);
     $funcionario1->__set("cargo", "Desenvolvedor");
     $funcionario1->__set("salario", 5000);
+    echo $funcionario1->resumeCadFunc();
+    
+    echo'<br>';
 
-    echo $funcionario1->__get("nome") . ' Possui ' . $funcionario1->__get("numFilhos") . ' filhos ' . " Salário: " . $funcionario1->__get("salario");
+    $funcionario2 = new GlobalFuncionario();
+    $funcionario2->__set("nome", "Jorge");
+    $funcionario2->__set("telefone", 3121);
+    $funcionario2->__set("numFilhos", 4);
+    $funcionario2->__set("cargo", "Sênior Desenvolvedor");
+    $funcionario2->__set("salario", 15000);
+    echo $funcionario2->resumeCadFunc();
+
+    
 ?>
