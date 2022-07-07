f2410366d0d66b10e4131a38b975d6aefc9b465e
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-07-07 16:29:43 -0300

    Seção 12 POO - Erros customizados "Excepetion"

---
+++ Seção 12 - POO/13 - Tratamento de Erros customizados/index.php
@@ -0,0 +1,24 @@
+<?php 
+    // Criando objeto customizado de erro
+
+    class MinhaExceptionCustomizada extends Exception{
+        private $erro = '';
+        public function __construct($erro)
+        {
+            $this->erro = $erro;
+        }
+
+        public function exibirMensagemErroCustomizada(){
+            echo '<div style="border: solid 1px #000; padding: 15px; background-color: red; color: white">';
+                echo $this->erro;
+            echo '</div>';
+        }
+    }
+    
+    try{
+        throw new MinhaExceptionCustomizada('Esse é um erro de test');
+
+    } catch(MinhaExceptionCustomizada $e){
+        $e->exibirMensagemErroCustomizada();
+    }
+?>
